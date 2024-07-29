b440fe3b3ab8e58e783cefbaf4c38fbf8541ea94
b1ruk <biruk3870@gmail.com>
2024-07-29 11:31:02 -0300

    add cors middleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors=require('cors');
+
 const applicationRoutes = require('./routes/applicationRoutes');
 
 require('dotenv').config();
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use('/api/applications', applicationRoutes);
